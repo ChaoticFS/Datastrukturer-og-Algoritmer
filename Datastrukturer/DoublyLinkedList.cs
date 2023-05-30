0bc0ce427d731f54040077ecf6776512e70727e6
Magnus Overgård <magnus.overgaard1711@gmail.com>
2023-05-30 20:07:30 +0200

    Progress

--- Datastrukturer/DoublyLinkedList.cs
+++ Datastrukturer/DoublyLinkedList.cs
@@ -1,1 +1,53 @@
-﻿// AddFirst(), AddLast(), RemoveFirst(), RemoveLast()
+﻿namespace datastructure;
+
+public class DoublyLinkedList
+{
+    internal DblNode? head;
+    internal DblNode? tail;
+
+    public DoublyLinkedList(int value)
+    {
+        head = new DblNode(value, null, null);
+        tail = head;
+    }
+
+    public void AddFirst(int value)
+    {
+        DblNode newHead = new DblNode(value, head, null);
+        head.prev = newHead;
+        head = newHead;
+    }
+
+    public void AddLast(int value)
+    {
+        DblNode newTail = new DblNode(value, null, tail);
+        tail.next = newTail;
+        tail = newTail;
+    }
+
+    public void RemoveFirst()
+    {
+        head.next.prev = null;
+        head = head.next;
+    }
+
+    public void RemoveLast()
+    {
+        tail.prev.next = null;
+        tail = tail.prev;
+    }
+}
+
+public class DblNode
+{
+    internal int data;
+    internal DblNode? next;
+    internal DblNode? prev;
+
+    public DblNode(int value, DblNode? _next, DblNode? _prev)
+    {
+        data = value;
+        next = _next;
+        prev = _prev;
+    }
+}
--- Datastrukturer/LinkedList.cs
+++ Datastrukturer/LinkedList.cs
@@ -1,1 +1,99 @@
-﻿// Implementer RemoveFirst(), GetLast(), Count, Contains
+﻿namespace datastructure;
+
+public class LinkedList
+{
+    Node? head;
+
+    public LinkedList(int value)
+    {
+        head = new Node(value);
+    }
+
+    public void Add(int value)
+    {
+        Node newNode = new Node(value, head);
+        head = newNode;
+    }
+
+    public void RemoveFirst()
+    {
+        if (head == null)
+        {
+            throw new IndexOutOfRangeException("No elements in list");
+        }
+
+        if (head.next == null)
+        {
+            head = null;
+        }
+
+        else
+        {
+            head = head.next;
+        }
+    }
+
+    public int Count()
+    {
+        int count = 0;
+        Node? next = head;
+
+        while (next is not null)
+        {
+            count++;
+            next = next.next;
+        }
+
+        return count;
+    }
+
+    public bool Contains(int targetVal)
+    {
+        Node? next = head;
+
+        while (next is not null)
+        {
+            if (next.data.Equals(targetVal))
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public int GetLast()
+    {
+        Node? next = head;
+
+        while (next is not null)
+        {
+            if (next.next is null)
+            {
+                return next.data;
+            }
+
+            next = next.next;
+        }
+
+        return -1;
+    }
+}
+
+public class Node
+{
+    internal int data;
+    internal Node? next;
+
+    public Node(int value)
+    {
+        data = value;
+        next = null;
+    }
+
+    public Node(int value, Node _next)
+    {
+        data = value;
+        next = _next;
+    }
+}
