cbbac93b8f36379438eea23ae6dde2c061a3e5a4
Magnus Overgård <magnus.overgaard1711@gmail.com>
2023-05-30 20:07:06 +0200

    Search algos

---
+++ Søgning/BinærSearch.cs
@@ -0,0 +1,30 @@
+﻿namespace search;
+public class BinærSearch
+{ // Fungerer kun på sorteret liste
+    public bool Search(List<int> list, int targetVal)
+    {
+        int L = 0;
+        int R = list.Count - 1;
+
+        while (L <= R)
+        {
+            int M = (L + R) / 2;
+
+            if (list[M] == targetVal)
+            {
+                return true;
+            }
+            else if (list[M] < targetVal) 
+            {
+                L = M + 1;
+            }
+            else
+            {
+                R = M - 1;
+            }
+
+        }
+
+        return false;
+    }
+}
---
+++ Søgning/LineærSearch.cs
@@ -0,0 +1,16 @@
+﻿namespace search;
+public class LineærSearch
+{ // Specifikt til numre, kan sagtens laves til andre typer uden større modifikation
+    public bool Search(List<int> list, int targetVal)
+    {
+        foreach (var item in list)
+        {
+            if (item == targetVal)
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
